22ea52b58c93b780169aa8718adf67f83b88297a
Martijn Vermaat <martijn@vermaat.name>
2014-07-07 15:22:01 +0200

    LUMC branding

--- app/helpers/appearances_helper.rb
+++ app/helpers/appearances_helper.rb
@@ -1,17 +1,27 @@
 module AppearancesHelper
   def brand_item
-    nil
+    true
   end
 
   def brand_title
-    'GitLab Community Edition'
+    'LUMC GitLab'
   end
 
   def brand_image
-    nil
+    image_tag 'lumc_logo.png'
   end
 
   def brand_text
-    nil
+    default_text =<<eos
+### Welcome to the LUMC GitLab server
+
+GitLab offers git repository management, code reviews, issue tracking,
+activity feeds and wikis. This server is free to use for LUMC employees and
+collaborators.
+
+[Read more about this service and how to use
+it](https://humgenprojects.lumc.nl/trac/humgenprojects/wiki/gitlab).
+eos
+    markdown default_text
   end
 end
